2d69b545bb0c573580aa4a44185c37067b75de43
at_sgalsi4 <at_sgalsi4@gl.miteco.gob.es>
2025-05-20 14:42:37 +0000

    actualizacion de nombre

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Companion Tools',
+      title: 'Cashly',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
