9e7e50ed4424e71a7ab0a920123cff76e4d92297
eneko12rebollo <eneko12rebollo@gmail.com>
2025-05-27 10:39:29 +0000

    adicion de firebase login con google

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:cashly/data/services/sqlite_service.dart';
 import 'package:cashly/onboarding/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:cashly/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SqliteService.initializeDatabase();
   runApp(const MyApp());
 }
